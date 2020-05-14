4ec1b3d0e18e9cdb05017a5d7249675873e04bf2
John C. Kennedy <devjckennedy@gmail.com>
2020-05-14 21:02:28 +0900

    correct typo

--- src/types.ts
+++ src/types.ts
@@ -5,27 +5,27 @@ export enum HashAlgo {
 }
 
 export interface HmacOTP {
-  /** Key: Unique shared secret key for encrypting C values for HMAC algorithm */
+  /** Key: Unique shared secret key for encrypting C (counter) values for HMAC algorithm */
   key: string
   /** Digits: Return digits of HOTP value, according to RFC4226, length >= 6 */
   digits?: number
-  /** Counter: 8-bytes incrementing counter value */
+  /** Counter: 8-byte incrementing counter value */
   counter: number
-  /** Hash Algorithm: HMAC Algorithm, is one of 'sha1', 'sha256' and 'sha512' */
+  /** Hash Algorithm: HMAC Algorithm to use; can be one of 'sha1', 'sha256' and 'sha512' */
   algorithm?: HashAlgo
 }
 
 export interface TimeOTP {
   /** T0: Unix time to start counting time steps */
   t0?: number
-  /** Key: Key for hashing */
+  /** Key: Shared secret used for encryption */
   key: string
   /** Digits: Return digits of TOTP value, according to RFC6238, length >= 6 */
   digits?: number
-  /** Epoch Time: Epoch time (Unix), defaults to current Unix time */
+  /** Epoch Time: Unix Epoch time, defaults to current Unix time */
   epochTime?: number
   /** Time Step X: Time step in seconds, default 30s */
   timeStepX?: number
-  /** Hash Algorithm: HMAC Algorithm, is one of 'sha1', 'sha256' and 'sha512' */
+  /** Hash Algorithm: HMAC Algorithm to use; can be one of 'sha1', 'sha256' and 'sha512' */
   algorithm?: HashAlgo
 }
