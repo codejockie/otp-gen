03cd13829dd31387e65ca5908b1a1507283d494b
John C. Kennedy <devjckennedy@gmail.com>
2020-05-19 00:02:49 +0900

    add more test cases

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -31,8 +31,33 @@ describe("hotp", () => {
 })
 
 describe("totp", () => {
-  test("returns time based otp", () => {
+  test("uses the default digits of 6", () => {
+    jest.spyOn(global.Date, "now").mockImplementationOnce(() => 1589613052907)
+    expect(totp({ algorithm: HashAlgo.SHA512, key: "testkey", t0: 1000 })).toEqual("182635")
+  })
+
+  test("allows custom return digits", () => {
     jest.spyOn(global.Date, "now").mockImplementationOnce(() => 1589613052907)
     expect(totp({ key: "testkey", digits: 10 })).toEqual("0173073020")
+  })
+
+  test("allows different hash algorithm", () => {
+    jest.spyOn(global.Date, "now").mockImplementationOnce(() => 1589613052907)
+    expect(totp({ algorithm: HashAlgo.SHA512, key: "testkey", digits: 10 })).toEqual("2134214641")
+  })
+
+  test("allows custom t0", () => {
+    jest.spyOn(global.Date, "now").mockImplementationOnce(() => 1589613052907)
+    expect(totp({ algorithm: HashAlgo.SHA512, key: "testkey", digits: 10, t0: 1000 })).toEqual("1508182635")
+  })
+
+  test("allows custom time step X", () => {
+    jest.spyOn(global.Date, "now").mockImplementationOnce(() => 1589613052907)
+    expect(totp({ algorithm: HashAlgo.SHA512, key: "testkey", digits: 8, timeStepX: 60 })).toEqual("53340704")
+  })
+
+  test("allows custom epoch time", () => {
+    jest.spyOn(global.Date, "now").mockImplementationOnce(() => 1589613052907)
+    expect(totp({ algorithm: HashAlgo.SHA512, digits: 8, epochTime: 1591714800, key: "testkey", timeStepX: 60 })).toEqual("83475233")
   })
 })
